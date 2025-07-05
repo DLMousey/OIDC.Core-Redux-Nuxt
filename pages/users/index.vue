43f991813c2f132cda6108fc2034a79474602cb6
DLMousey <tmlovelady92@gmail.com>
2025-07-05 12:37:43 +0100

    Switch user list page to nuxt-ui

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -5,12 +5,67 @@ import type IUser from "~/data/models/IUser";
 import type IAuthRefreshResponse from "~/data/models/IAuthRefreshResponse";
 import ErrorCard from "~/components/core/ErrorCard.vue";
 import {useFetchWithRefresh} from "~/composables/fetch-refresh";
+import {UCheckbox} from "#components";
+import type {TableColumn} from "#ui/components/Table.vue";
+import {getPaginationRowModel} from "@tanstack/table-core";
 
 const loading = ref(true);
 const users = ref(null);
 
 const fetchConfig = useFetchConfig(`/users`, { server: false, lazy: true });
 const { data, error } = await useFetchWithRefresh<IUser[]>(fetchConfig.url, fetchConfig.config);
+
+const columns: TableColumn<IUser>[] = [{
+  id: 'select',
+  header: ({ table }) => h(UCheckbox, {
+    'modelValue': table.getIsSomePageRowsSelected() ? 'indeterminate' : table.getIsAllPageRowsSelected(),
+    'onUpdate:modelValue': (value: boolean | 'indeterminate') => table.getIsAllPageRowsSelected(!!value),
+    'aria-label': 'Select all'
+  }),
+  cell: ({ row }) => h(UCheckbox, {
+    'modelValue': row.getIsSelected(),
+    'onUpdate:modelValue': (value: boolean | 'indeterminate') => row.toggleSelected(!!value),
+    'aria-label': 'Select row'
+  }),
+  enableSorting: false,
+  enableHiding: true
+}, {
+  accessorKey: 'id',
+  header: 'ID (GUID)',
+  cell: ({ row }) => `${row.getValue('id')}`
+}, {
+  accessorKey: 'username',
+  header: 'Username',
+  cell: ({ row }) => `${row.getValue('username')}`
+}, {
+  accessorKey: 'email',
+  header: 'Email address',
+  cell: ({ row }) => `${row.getValue('email')}`,
+  enableSorting: true,
+  enableHiding: true
+}, {
+  accessorKey: 'applications',
+  header: 'Published applications',
+  cell: ({ row }) => 'TODO: Applications',
+  enableSorting: true,
+  enableHiding: false
+}, {
+  accessorKey: 'accessToken',
+  header: 'Access Tokens',
+  cell: ({ row }) => 'TODO: Access tokens',
+  enableSorting: true,
+  enableHiding: false
+}, {
+  id: 'action',
+  header: 'Actions'
+}];
+
+const table = useTemplateRef('table');
+const globalFilter = ref();
+const pagination = ref({
+  pageIndex: 0,
+  pageSize: 10
+});
 
 if (data) {
   loading.value = false;
@@ -19,139 +74,35 @@ if (data) {
 </script>
 
 <template>
-<div class="users">
-  <div class="users-toolbar">
-    <NuxtLink to="/users/create" class="button">Create User</NuxtLink>
+  <div class="flex-1 divide-y divide-accented w-full">
+    <div class="flex px-4 py-3.5 border-b border-accented">
+      <UInput v-model="globalFilter" class="max-w-sm" placeholder="Search users..." />
+    </div>
+    <UTable ref="table"
+            v-model:global-filter="globalFilter"
+            v-model:pagination="pagination"
+            :pagination-options="{getPaginationRowModel: getPaginationRowModel()}"
+            :data="data"
+            :columns="columns"
+            sticky
+            class="h-96"
+    >
+      <template #expanded="{ row }">
+        <pre>{{ row.original }}</pre>
+      </template>
+      <template #action-cell="{ row }">
+        <NuxtLink :to="{ name: 'users-id', params: { id: row.getValue('id') }}">
+          <UButton color="neutral" variant="outline">Edit User</UButton>
+        </NuxtLink>
+      </template>
+    </UTable>
+    <div class="flex justify-center border-t border-default pt-4">
+      <UPagination
+        :default-page="(table?.tableApi?.getState().pagination.pageIndex || 0) + 1"
+        :items-per-page="table?.tableApi?.getState().pagination.pageSize"
+        :total="table?.tableApi?.getFilteredRowModel().rows.length"
+        @update:page="(p) => table?.tableApi.setPageIndex(p - 1)"
+      />
+    </div>
   </div>
-  <div class="table-container">
-    <table class="table" v-if="!loading && data">
-      <thead>
-      <tr>
-        <th>ID</th>
-        <th>Username</th>
-        <th>Email</th>
-        <th>Applications</th>
-        <th>Access Tokens</th>
-        <th></th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="user in data" :key="user.id">
-        <td>{{ user.id }}</td>
-        <td>{{ user.username }}</td>
-        <td><a :href="'mailto:'+user.email">{{ user.email }}</a></td>
-        <td>0</td>
-        <td>0</td>
-        <td>
-          <div class="button-group">
-            <button class="button">Edit</button>
-            <button class="button">Deactivate</button>
-          </div>
-        </td>
-      </tr>
-      </tbody>
-    </table>
-  </div>
-</div>
 </template>
-
-<style scoped lang="scss">
-//.users {
-//  display: flex;
-//  flex-direction: column;
-//  background: #FFF;
-//  width: 100%;
-//  height: 100%;
-//
-//  &-list {
-//    display: flex;
-//    flex-wrap: wrap;
-//    width: 100%;
-//    height: 100%;
-//  }
-//
-//  &-listitem {
-//    flex-basis: 30%;
-//    height: 275px;
-//  }
-//
-//  &-toolbar {
-//    display: flex;
-//    justify-content: flex-end;
-//    padding: 1em;
-//    border-bottom: 1px solid #EBEBEB;
-//    box-shadow: 0px 5px 25px 0px rgba(0,0,0,0.1);
-//  }
-//}
-//
-//.table-container {
-//  display: flex;
-//  flex-direction: column;
-//}
-//
-//.table {
-//  //width: 100%;
-//  //border-collapse: collapse;
-//  //border: 1px solid red;
-//  margin: 1em;
-//
-//  thead, tbody {
-//    width: 100%;
-//  }
-//
-//  tr, th, td {
-//    text-align: left;
-//  }
-//
-//  th {
-//    font-weight: bold;
-//    padding: 1em 0 1em 0.5em;
-//    border-bottom: 1px solid #DDD;
-//  }
-//
-//  td {
-//    padding: 1em 0 1em 0.5em;
-//    border-bottom: 1px solid #DDD;
-//
-//    &:last-child {
-//      padding-right: 1em;
-//    }
-//
-//    .button {
-//      padding: 0.8em 0.25em;
-//      margin: 0;
-//      text-transform: uppercase;
-//      font-weight: bold;
-//      font-size: 12px;
-//    }
-//  }
-//
-//  tr {
-//    &:hover {
-//      cursor: pointer;
-//      background: #EEE;
-//    }
-//  }
-//
-//  .button-group {
-//    display: flex;
-//
-//    .button:first-child {
-//      border-top-right-radius: 0;
-//      border-bottom-right-radius: 0;
-//      border-right: 1px solid black;
-//    }
-//    .button:last-child {
-//      border-top-left-radius: 0;
-//      border-bottom-left-radius: 0;
-//      border-left: 1px solid black;
-//    }
-//
-//    .button:not(:first-child):not(:last-child) {
-//      border-radius: 0;
-//      border-left: 1px solid black;
-//      border-right: 1px solid black;
-//    }
-//  }
-//}
-</style>
